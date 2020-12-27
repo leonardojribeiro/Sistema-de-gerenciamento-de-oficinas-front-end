cf237305ba39a1d75a419fe128f33f5a7508723b
Leonardo Jardim Ribeiro <leonardojr410@gmail.com>
2020-12-27 19:34:27 -0300

    agrupamento do histórico do veículo por vínculo

--- src/pages/Veiculo/HistoricoVeiculo/index.tsx
+++ src/pages/Veiculo/HistoricoVeiculo/index.tsx
@@ -1,21 +1,60 @@
-import { Box, Grid, Typography } from '@material-ui/core';
+import { Avatar, Box, Divider, Grid, List, ListItem, ListItemAvatar, ListItemText, Typography } from '@material-ui/core';
 import React, { useCallback, useContext, useEffect, useState } from 'react';
 import BotaoIncluir from '../../../componentes/BotaoIncluir';
 import Dialog from '../../../componentes/Dialog';
 import ApiContext from '../../../contexts/ApiContext';
 import useQuery from '../../../hooks/useQuery';
 import OrdemDeServico from '../../../Types/OrdemDeServico';
+import Vinculo from '../../../Types/Vinculo';
 import ItemOrdemDeServico from '../../OrdemDeServico/ItemOrdemDeServico';
+import PersonIcon from '@material-ui/icons/Person';
+import Formato from '../../../recursos/Formato';
+
+interface AgrupamentoVinculoOrdemDeServico {
+  vinculo: Vinculo;
+  ordensDeServico: OrdemDeServico[];
+}
 
 const HistoricoVeiculo: React.FC = () => {
   const veiculo = useQuery('veiculo');
-  const [ordensDeServico, setOrdensDeServico] = useState<OrdemDeServico[]>();
+  const [agrupamento, setAgrupamento] = useState<AgrupamentoVinculoOrdemDeServico[]>()
   const { get } = useContext(ApiContext);
 
   const listar = useCallback(async () => {
-    const resposta = await get(`/ordemdeservico/veiculo?veiculo=${veiculo}`) as any;
-    if (resposta) {
-      setOrdensDeServico(resposta.ordensDeServico as OrdemDeServico[]);
+    const vinculos = await get(`/veiculo/consultaVinculo?veiculo=${veiculo}`) as any;
+    if (vinculos) {
+      let vinculos0 = vinculos.vinculos as Vinculo[];
+
+      vinculos0 = vinculos0.sort((a, b) => {
+        if (new Date(a.vinculoInicial).getTime() < new Date(b.vinculoInicial).getTime()) {
+          return 1;
+        }
+        if (new Date(a.vinculoInicial).getTime() > new Date(b.vinculoInicial).getTime()) {
+          return -1;
+        }
+        return 0;
+      })
+      const resposta = await get(`/ordemdeservico/veiculo?veiculo=${veiculo}`) as any;
+      const oss = resposta.ordensDeServico as OrdemDeServico[];
+      const agr: AgrupamentoVinculoOrdemDeServico[] = [];
+      vinculos0.forEach((vinculo, index) => {
+        agr.push({ vinculo, ordensDeServico: [] });
+        oss.forEach((os) => {
+          if (vinculo.vinculoFinal !== undefined) {
+            if (new Date(os.dataDeRegistro).getTime() >= new Date(vinculo.vinculoInicial).getTime() && new Date(os.dataDeRegistro).getTime() < new Date(vinculo.vinculoFinal).getTime()) {
+              agr[index].ordensDeServico.push(os);
+            }
+          }
+          else {
+            if (new Date(os.dataDeRegistro).getTime() >= new Date(vinculo.vinculoInicial).getTime()) {
+              agr[index].ordensDeServico.push(os);
+            }
+          }
+        })
+      })
+
+      console.log(agr);
+      setAgrupamento(agr);
     }
   }, [get, veiculo]);
 
@@ -23,18 +62,42 @@ const HistoricoVeiculo: React.FC = () => {
     listar();
   }, [listar]);
 
-
   return (
-    <Dialog title={ordensDeServico ? `Histórico do veículo ${ordensDeServico[0]?.veiculo.placa.toLocaleUpperCase()}` : ""} open fullScreen>
+    <Dialog title={`Histórico do veículo `} open fullScreen>
       <Box mb={2}>
-        <Grid container spacing={3} justify="center">
-          <Box p={2}>
-            <Typography variant="h6">Ordens de Serviço efetuadas neste veículo:</Typography>
-          </Box>
-          {ordensDeServico?.map((ordemDeServico, index) => (
-            <ItemOrdemDeServico ordemDeServico={ordemDeServico} key={index} />
-          ))}
-        </Grid>
+        <List>
+          {
+            agrupamento?.map((agrupamento, index) => (
+              <Grid container key={index} spacing={2} justify="center" >
+                <Grid item xs={12} sm={11} lg={10} xl={8}>
+                  <ListItem>
+                    <ListItemAvatar>
+                      <Avatar>
+                        <PersonIcon />
+                      </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText
+                      primary={`${agrupamento.vinculo.cliente.nome}`}
+                      secondary={`Desde: ${Formato.formatarData(agrupamento.vinculo.vinculoInicial)} até ${agrupamento.vinculo.vinculoFinal ? Formato.formatarData(agrupamento.vinculo.vinculoFinal) : "o momento"}.`}
+                    />
+                  </ListItem>
+                </Grid>
+                {agrupamento.ordensDeServico.length > 0
+                  ? agrupamento.ordensDeServico.map((ordemDeServico, index) => (
+                    <ItemOrdemDeServico ordemDeServico={ordemDeServico} key={index} />
+                  ))
+                  : (
+                    <Grid item xs={12} sm={11} lg={10} xl={8}>
+                      <Typography align="center" variant="h6">Não houveram ordens de serviço nesse período!</Typography>
+                    </Grid>
+                  )}
+                <Grid item xs={12} sm={11} lg={10} xl={8}>
+                  <Divider />
+                </Grid>
+              </Grid>
+            ))
+          }
+        </List>
       </Box>
       <BotaoIncluir titulo="incluir ordem de serviço para este veículo" linkTo={`/ordensdeservico/incluir?veiculo=${veiculo}`} />
     </Dialog>
